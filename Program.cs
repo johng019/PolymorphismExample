4412fbf9d1e96d71400bc2e795322ee36f6e87a3
gary0958 <gljfb@yahoo.com>
2017-01-31 15:58:56 -0500

    Polymorphism example

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphism
+{
+    public class BaseClass
+    { 
+
+        public virtual void printInfo()
+        {
+
+            Console.WriteLine("This is the BaseClass");
+        }
+    }
+    public class DerivedClass_1 : BaseClass
+    { 
+
+        public override void printInfo()
+        {
+             Console.WriteLine("This is the Derrived_1 Class");
+        }
+    }
+    public class DerivedClass_2 : BaseClass
+    {
+        public override void printInfo()
+        {
+            Console.WriteLine("This is the Derrived_2 Class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            BaseClass[] bc = new BaseClass[3];
+            bc[0] = new DerivedClass_1();
+            bc[1] = new DerivedClass_2();
+            bc[2] = new BaseClass();
+
+            foreach(BaseClass item  in bc)
+            {
+                item.printInfo();
+            }
+            Console.ReadKey();
+        }
+    }
+}
